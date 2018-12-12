df59672fbbcefda6816609b10f58039d1d6a12ab
Dylan Powers <ddp19@duke.edu>
2018-12-12 15:38:39 -0500

    v1 with 2 stops and 1 route working

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 'use strict';
-const https = require('https');
-const Alexa = require('ask-sdk-core');
+const request = require('request');
+const Alexa = require('ask-sdk');
+const url = require('url');
 
+// setup ===================================
 const stopMap = new Map();
-const GET_URL = 'https://transloc-api-1-2.p.mashape.com/arrival-estimates.json?agencies=176&callback=call&stops=';
+stopMap.set('west', 4146366); // Duke Chapel
+stopMap.set('east', 4117202); // east campus bus stop
+
+// const GET_URL = 'https://transloc-api-1-2.p.mashape.com/arrival-estimates.json?agencies=176&callback=call&stops=';
 
 // if the user speaks something like "Alexa, open Duke Transloc"
 const LaunchRequestHandler = {
@@ -34,12 +39,22 @@ const IntentRequestHandler = {
   },
 
   async handle(handlerInput) {
-    let slots = handlerIntent.requestEnvelope.request.intent.slots;
+    let slots = handlerInput.requestEnvelope.request.intent.slots;
     let busName = slots.bus.value;
     let stopName = slots.stop.value.toLowerCase();
+    if (!stopMap.has(stopName)) {
+      // stop is not recognized. Tell the user, but do not exit
+      let noStopMessage = stopName + ' is not a stop that I recognize. Please try again.';
+      return handlerInput.responseBuilder
+        .speak(noStopMessage)
+        .withShouldEndSession(false)
+        .getResponse();
+    }
+
+    console.log(slots);
 
     // get actual arrival time with https request
-    const message = await getArrivalTimes(bus, stop);
+    const message = await getArrivalTimes(busName, stopName);
     return handlerInput.responseBuilder
       .speak(message)
       .withShouldEndSession(true)
@@ -48,37 +63,47 @@ const IntentRequestHandler = {
 };
 
 const ErrorHandler = {
+
   canHandle() {
     return true;
   },
+
   handle(handlerInput, error) {
+    let errorMessage = 'Sorry, I can\'t understand the command. Please try again.';
+    console.log(error);
 
     return handlerInput.responseBuilder
-      .speak('Sorry, I can\'t understand the command. Please say again.')
-      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .speak(error.message)
+      .reprompt(error.message)
       .getResponse();
-  },
+  }
 };
 
 /**
  * Makes https request to Mashape in order to get next arrival time.
  * Time difference processing done in another function
  */
-async function getArrivalTimes(bus, stop) {
-  const message;
-  if (!stopMap.has(stop)) {
-   message = stopName + " is not a stop that I recognize. Please try again";
-  } else {
+function getArrivalTimes(bus, stop) {
+  return new Promise((resolve, reject) => {
+
+    let message;
     const options = {
-      'X-Mashape-Key': mashapeKey,
-      'Accept': 'application/json'
+      url: 'https://transloc-api-1-2.p.mashape.com/arrival-estimates.json?agencies=176&callback=call&stops=' + stopMap.get(stop),
+      method: 'GET',
+      headers: {
+        'X-Mashape-Key': process.env.mashapeKey,
+        'Accept': 'application/json'
+      }
     };
-    await https.get(GET_URL + stopMap.get(stop),
-      options, (res) => {
-        message = processData(res, stop);
-      });
-  }
-  return message;
+    request(options, (error, response, body) => {
+      if (error) {
+        console.log(error);
+        reject(error);
+      }
+      message = processData(JSON.parse(body), stop);
+      resolve(message);
+    });
+  });
 };
 
 /**
@@ -86,7 +111,7 @@ async function getArrivalTimes(bus, stop) {
  * next arrival time. Returns String that should be spoken
  */
 function processData(res, stopName) {
-  let nextArrival = res.data[0][0];
+  let nextArrival = res.data[0]['arrivals'][0];
   let now = new Date();
   let arrivalTime = new Date(nextArrival['arrival_at']);
   const minutes = arrivalTime.getMinutes() - now.getMinutes();
